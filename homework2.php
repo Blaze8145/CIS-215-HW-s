c765b16e01493e8fc6f61fbc230bcb43abea93d0
Blaze8145 <149420309+Blaze8145@users.noreply.github.com>
2026-02-04 23:59:21 +0000

    feat: added time php code

--- homework2.php
+++ homework2.php
@@ -6,6 +6,14 @@ echo "<title>HW2</title>";
 echo "</head>";
 echo "<body>";
 echo "<h1>Hello World!</h1>";
+$time = date(12);
+if(6 < $time && $time < 12){
+    echo "Morning";
+}elseif($time < 18){
+    echo "Afternoon";
+}else{
+    echo "Night";
+}
 echo "</body>";
 echo "</html";
 ?>

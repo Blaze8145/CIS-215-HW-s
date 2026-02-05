28678c5caf52b083a74db5a29350e0e3e77e83af
Blaze8145 <149420309+Blaze8145@users.noreply.github.com>
2026-02-05 00:12:52 +0000

    fix: date from int to string

--- homework2.php
+++ homework2.php
@@ -6,8 +6,8 @@ echo "<title>HW2</title>";
 echo "</head>";
 echo "<body>";
 echo "<h1>Hello World!</h1>";
-$time = date(12);
-if(6 < $time && $time < 12){
+$time = date("t");
+if(6 <= $time && $time < 12){
     echo "Morning";
 }elseif($time < 18){
     echo "Afternoon";

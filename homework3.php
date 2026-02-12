cf0b5172dfbd64d10c0abd6b9496228d32029853
Blaze8145 <149420309+Blaze8145@users.noreply.github.com>
2026-02-12 18:59:30 +0000

    feet added functions letterGrade and Fizzbuzz and added comments at start

--- homework3.php
+++ homework3.php
@@ -1,4 +1,10 @@
 <?php
+    /*
+    Author:Julian Schrauger jschraug@genesee.edu
+    The function loops displays both for loop and while loops of two count up loops
+    The function letterGrade receives a number and turns it into a letter grade based on a ifelse chain
+
+    */
     function loops(){
         // This for loop counts from 1 to $n and adds only factors of 2 into the array $evens
         $evens = array();
@@ -12,8 +18,8 @@
        $whileMax =10;
        $count = 1;
        //This while loop counts from 1 to $count and adds only factors of 2 into the array $whileEvens
-       while($count <= $count){
-            if($num%2 == 0){
+       while($count <= $whileMax){
+            if($count%2 == 0){
                 $whileEvens[]=$num;
             }
             $count++;
@@ -29,12 +35,45 @@
         //While loop function that count 1 to 30 and prints numbers divisable of 2 and 3
         $divisables = 1;
         while($divisables<=30){
-            if($factorial%2==0){
-                echo $factorial . " ";
-            }elseif($factorial%3==0){
-                echo $factorial . " ";
+            if($divisables%2==0){
+                echo $divisables . " ";
+            }elseif($divisables%3==0){
+                echo $divisables . " ";
             }
             $divisables++;
         }
     }
+    loops();
+    /*This Function recieves a number to turn into a letter grade
+    */
+    $grade =0;
+    function letterGrade($grade){
+        if($grade >= 90){
+            return "A";
+        }elseif($grade >= 80){
+            return "B";
+        }elseif($grade >= 70){
+            return "C";
+        }elseif($grade >= 60){
+            return "D";
+        }else{
+            return "F";
+        }
+    }
+    echo letterGrade(80);
+    $stopNum=0;
+    function FizzBuzz($stopNum){
+        for($start =1;$start<=$stopNum;$start++){
+            if($start%3==0&&$start%5==0){
+                echo "FizzBuzz";
+            }elseif($start%3==0){
+                echo "Fizz";
+            }elseif($start%5==0){
+                echo "Buzz";
+            }else{
+                echo $start;
+            }
+        }
+    }
+    echo FizzBuzz(15);
 ?>

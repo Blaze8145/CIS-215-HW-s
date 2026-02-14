6551b41c2f84b86f7a7dade2302c088470e5e149
Blaze8145 <149420309+Blaze8145@users.noreply.github.com>
2026-02-14 18:34:45 +0000

    feat Get Post add

--- Form.php
+++ Form.php
@@ -12,12 +12,12 @@
                 <input type="email" name="getEmail" id="email_ID" /></div>
             <fieldset>
             <legend>What's your favorite MTG color combo?</legend>
-            <div><input type="checkbox" name="MTG color combo" id="color-white" value="white" /><label for="color-white">White</label></div>
-            <div><input type="checkbox" name="MTG color combo" id="color-blue" value="blue"/><label for="color-blue">Blue</label></div>
-            <div><input type="checkbox" name="MTG color combo" id="color-black" value="black"/><label for="color-black">Black</label></div>
-            <div><input type="checkbox" name="MTG color combo" id="color-red" value="red"/><label for="color-red">Red</label></div>
-            <div><input type="checkbox" name="MTG color combo" id="color-green" value="green"/><label for="color-green">Green</label></div>
-            <div><input type="checkbox" name="MTG color combo" id="color-colorless" value="colorless"/><label for="color-colorless">Colorless</label></div>
+            <div><input type="checkbox" name="MTG color combo[]" id="color-white" value="white" /><label for="color-white">White</label></div>
+            <div><input type="checkbox" name="MTG color combo[]" id="color-blue" value="blue"/><label for="color-blue">Blue</label></div>
+            <div><input type="checkbox" name="MTG color combo[]" id="color-black" value="black"/><label for="color-black">Black</label></div>
+            <div><input type="checkbox" name="MTG color combo[]" id="color-red" value="red"/><label for="color-red">Red</label></div>
+            <div><input type="checkbox" name="MTG color combo[]" id="color-green" value="green"/><label for="color-green">Green</label></div>
+            <div><input type="checkbox" name="MTG color combo[]" id="color-colorless" value="colorless"/><label for="color-colorless">Colorless</label></div>
             </fieldset>
             <fieldset>
              <legend>What's your favorite MTG color?</legend>
@@ -52,6 +52,27 @@
                 <label for="long_text">Explain why you like the MTG color you chose in a sentence</label>
                 <textarea id="long_text" name="long_text answer" rows="3" cols="40">I like how green can ramp</textarea>
             </div>
+            <fieldset>
+                <label>What is your favorite square Number 1-100</label>
+                <?php
+                    for($sq=1;$sq<=100;$sq++){
+                        $sqed=$sq*$sq;
+                        print("<div><input type='radio' name='squared' id='square_of_$sq' value ='$sq'>
+                        <label for='square_of_$sq'>$sqed</label></div>");
+                    }
+                ?>
+            </fieldset>
+            <fieldset><label>Favorite First 10 prime numbers</label>
+                <?php
+                    $primes=array(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+                    for($n=0;$n<count($primes);$n++){
+                            print("<div>
+                            <input type='radio' name='prime' id='prime_$n' value='$n'>
+                            <label for='prime_$n'>$primes[$n]</label>
+                            </div>");    
+                    }
+                ?>
+            </fieldset>
 
                 <button typr="submit" name="submit-button" id="sumbit_ID">Submit</button>
         </form>
--- confimation.php
+++ confimation.php
@@ -1,4 +1,7 @@
 <?php
 var_dump($_POST["MTG color combo"],
         $_POST["MTG color"]);
+        if($_POST["MTG color combo"]=="green"){
+                echo "Its my favorite too.";
+        }
 ?>

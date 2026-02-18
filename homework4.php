9c40358bc7613b65a4c1e43c1c1ab21d5747b932
Blaze8145 <149420309+Blaze8145@users.noreply.github.com>
2026-02-18 21:16:47 +0000

    feat made basic layout of assignment

---
+++ homework4.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>Form Example</title>
+    </head>
+<body>
+    <main>
+        <form action="confimation.php" method="POST">
+            <div>
+                <label for="email_ID">Enter email Here:</label>
+                <input type="email" name="getEmail" id="email_ID" /></div>
+            <fieldset><legend></legend></fieldset>
+            <fieldset><legend></legend></fieldset>
+            <fieldset><legend></legend></fieldset>
+            <fieldset><legend></legend></fieldset>
+            <fieldset><legend></legend></fieldset>
+            <fieldset><legend></legend></fieldset>
+            <fieldset><legend></legend></fieldset>
+            <fieldset><legend></legend></fieldset>
+            <fieldset><legend></legend></fieldset>
+            <fieldset><legend></legend></fieldset>
+            <button typr="submit" name="submit-button" id="sumbit_ID">Submit</button>
+        </form>
+    </main>    
+</body>
+</html>

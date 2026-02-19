20a13e4c8cdc88a4a03cee94f366d0ae0d197e3b
Blaze8145 <149420309+Blaze8145@users.noreply.github.com>
2026-02-19 04:20:11 +0000

    feat andded function winner

--- confirmation.php
+++ confirmation.php
@@ -11,9 +11,43 @@
             exit();
         }
     }
+    //Holds counters for end result
     $agro = 0;
     $stax = 0;
     $combo = 0;
     $midrange = 0;
     $politic = 0;
+    $result=[$_POST["MTG Win"],$_POST["MTG Hand"],$_POST["MTG Threat"],$_POST["MTG Deck"],$_POST["MTG Games"],$_POST["MTG Topdeck"],$_POST["MTG Table"],$_POST["MTG Card"]];
+    foreach($result as $point){
+        if($point == "agro"){
+            $agro++;
+        }elseif($point == "stax"){
+            $stax++;
+        }elseif($point =="combo"){
+            $combo++;
+        }elseif($point == "midrange"){
+            $midrange++;
+        }elseif($point == "politic"){
+            $politic++;
+        }else{
+            echo "ERR: Invalid Answer";
+            exit();
+        }
+    }
+    //This Function makes is so it will go thruw every player and see which one is the highest.
+    
+    function winner($agro, $stax, $combo, $midrange, $politic){
+        if($agro >= $stax&&$agro >= $combo&& $agro >= $midrange && $agro >= $politic){
+            echo"<p>You are a Agro Player!</p>";
+        }elseif($stax >= $agro&&$stax >= $combo&& $stax >= $midrange && $stax >= $politic){
+            echo"<p>You are a Stax Player!</p>";
+        }elseif($combo >= $stax&&$combo >= $agro&& $combo >= $midrange && $combo >= $politic){
+            echo"<p>You are a Combo Player!</p>";
+        }elseif($midrange >= $stax&&$midrange >= $combo&& $midrange >= $agro && $midrange >= $politic){
+            echo"<p>You are a Midrange Player!</p>";
+        }elseif($politic >= $stax&&$politic >= $combo&& $politic >= $midrange && $politic >= $agro){
+            echo"<p>You are a Politic Player!</p>";
+        }
+    }
+    winner($agro, $stax, $combo, $midrange, $politic);
 ?>

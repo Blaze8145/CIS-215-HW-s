e60440ac962d50fa1dbdb073f31df26512e8a1e1
Blaze8145 <149420309+Blaze8145@users.noreply.github.com>
2026-02-28 19:50:29 +0000

    fetch

--- config.php
+++ config.php
@@ -1,14 +1,20 @@
 <?php
-require_once("dbconfig.php");
-$db = connectDB();
+//require_once("dbconfig.php");
+//$db = connectDB();
 /*$db->query("CREATE TABLE example(id INT AUTO_INCREMENT PRIMARY KEY, anotherColemn VARCHAR(50) NOT NULL, anotherInt INT);
 INSERT INTO example(anotherColemn,anotherInt) VALUES('Hey',2);
 ");*/
-$city = $_POST["city"];
-$price = $_POST["price"];
-$days = $_POST["daysListed"];
-$prepared_stat = $db->prepare("INSERT INTO houses(houseCity,price,daysListed) VALUES(?,?,?);");
-$prepared_stat->execute(array($city,$price,$days));
+//$city = $_POST["city"];
+//$price = $_POST["price"];
+//$days = $_POST["daysListed"];
+//$prepared_stat = $db->prepare("INSERT INTO houses(houseCity,price,daysListed) VALUES(?,?,?);");
+//$prepared_stat->execute(array($city,$price,$days));
 //$exampleInput = $_POST["example"];
 //$prepared_stat->execute(array($exampleInput));
+$select_data = $db->prepare("SELECT * FROM pets;");
+$select_data->execute();
+
+$fetch1 = $select_data->fetch();
+$fetchall = $select_data->fetchall();
+vardump($fetch1,$fetchall);
 ?>

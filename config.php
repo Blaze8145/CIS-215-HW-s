d7492b1ab5035ebfd243ea60223ced59bc288d51
Blaze8145 <149420309+Blaze8145@users.noreply.github.com>
2026-02-28 19:58:11 +0000

    fix

--- config.php
+++ config.php
@@ -4,11 +4,11 @@ require_once("dbconfig.php");
 /*$db->query("CREATE TABLE example(id INT AUTO_INCREMENT PRIMARY KEY, anotherColemn VARCHAR(50) NOT NULL, anotherInt INT);
 INSERT INTO example(anotherColemn,anotherInt) VALUES('Hey',2);
 ");*/
-//$city = $_POST["city"];
-//$price = $_POST["price"];
-//$days = $_POST["daysListed"];
-//$prepared_stat = $db->prepare("INSERT INTO houses(houseCity,price,daysListed) VALUES(?,?,?);");
-//$prepared_stat->execute(array($city,$price,$days));
+$city = $_POST["city"];
+$price = $_POST["price"];
+$days = $_POST["daysListed"];
+$prepared_stat = $db->prepare("INSERT INTO houses(houseCity,price,daysListed) VALUES(?,?,?);");
+$prepared_stat->execute(array($city,$price,$days));
 //$exampleInput = $_POST["example"];
 //$prepared_stat->execute(array($exampleInput));
 $select_data = $db->prepare("SELECT * FROM pets;");

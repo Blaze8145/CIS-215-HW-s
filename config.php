7adeaf68baf9c87a8497b3738b55120e8beebac1
Blaze8145 <149420309+Blaze8145@users.noreply.github.com>
2026-02-28 19:59:57 +0000

    fix

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 require_once("dbconfig.php");
-//$db = connectDB();
+$db = connectDB();
 /*$db->query("CREATE TABLE example(id INT AUTO_INCREMENT PRIMARY KEY, anotherColemn VARCHAR(50) NOT NULL, anotherInt INT);
 INSERT INTO example(anotherColemn,anotherInt) VALUES('Hey',2);
 ");*/

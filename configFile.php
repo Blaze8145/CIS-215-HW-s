aef3362bc53ca18500b2bb33690ffacd6ecdd89a
Blaze8145 <149420309+Blaze8145@users.noreply.github.com>
2026-02-28 18:48:06 +0000

    feat added Prepared Statements

--- configFile.php
+++ configFile.php
@@ -1,6 +1,10 @@
 <?php
 $db = connectDB();
-$db->query("CREATE TABLE example(id INT AUTO_INCREMENT PRIMARY KEY, anotherColemn VARCHAR(50) NOT NULL, anotherInt INT);
+/*$db->query("CREATE TABLE example(id INT AUTO_INCREMENT PRIMARY KEY, anotherColemn VARCHAR(50) NOT NULL, anotherInt INT);
 INSERT INTO example(anotherColemn,anotherInt) VALUES('Hey',2);
-");
+");*/
+$prepared_stat = $db->prepare("INSERT INTO example(anotherColemn,anotherInt) VALUES(?,?);");
+$prepared_stat->execute(array($col1_input,$col2_input));
+$exampleInput = $_POST["example"];
+$prepared_stat->execute(array($exampleInput));
 ?>

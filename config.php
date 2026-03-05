975c24e83b426cd47112a3087e2f1eba44d3cf81
Blaze8145 <149420309+Blaze8145@users.noreply.github.com>
2026-03-05 02:46:23 +0000

    feat added start of fetch

--- config.php
+++ config.php
@@ -19,53 +19,54 @@ $movie->bind_param('ssiss',$title,$rating,$runtime,$director,$dateOfRelease);
 //Personal rating 11/10
 $title="A Silent Voice";
 $rating="PG-13";
-$runtime="130";
+$runtime=130;
 $director="Naoko Yamada";
 $dateOfRelease="2016-09-17";
-movie->execute();
+$movie->execute();
 
 //Monty Python and the Holy Grail
 //Personal Rating 8/10
 $title="Monty Python and the Holy Grail";
 $rating="PG";
-$runtime="91";
+$runtime=91;
 $director="Terry Gilliam";
 $dateOfRelease="1975-04-03";
-movie->execute();
+$movie->execute();
 
 //Back to the Future
 //Personal Rating 7/10
 $title="Back to the Future";
 $rating="PG";
-$runtime="116";
+$runtime=116;
 $director="Robert Zemeckis";
 $dateOfRelease="1985-07-03";
-movie->execute();
+$movie->execute();
 
 //Iron Lung
 //Personal Rating N/A Plan to see it
 $title="Iron Lung";
 $rating="R";
-$runtime="110";
+$runtime=110;
 $director="Mark Fischbach";
 $dateOfRelease="2024-01-01";
-movie->execute();
+$movie->execute();
 
 //SpaceBalls
 //Personal Rating 9/10
 $title="SpaceBalls";
 $rating="R";
-$runtime="96";
+$runtime=96;
 $director="Mel Brooks";
 $dateOfRelease="1987-06-24";
-movie->execute();
+$movie->execute();
 
 //Gets Data from Movies
 $select_data = $db->prepare("SELECT movieName FROM movies WHERE runtime < 120;");
 $select_data->execute();
+$result = $select_data->fetchall;
 
 //Fetch data
-$fetch1 = $select_data->fetch();
-$fetchall = $select_data->fetchall();
-var_dump($fetch1,$fetchall);
+foreach($result as $movie){
+    echo "<div></div>";
+}
 ?>

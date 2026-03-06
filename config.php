d1070ec59abdd10374f8c48e4d807076b61ade70
Blaze8145 <149420309+Blaze8145@users.noreply.github.com>
2026-03-06 15:36:58 +0000

    fix err of not being able to insert

--- config.php
+++ config.php
@@ -12,61 +12,34 @@ $db->query(
  dateRelease DATE)");
 //Sets up Varible to add values into table.
 $movie = $db->prepare("INSERT INTO movies(movieName,rating,runtime,director,dateRelease) VALUES(?,?,?,?,?);");
-//bind_param makes it so the order of inputs is String String Int String Sting
-$movie->bind_param('ssiss',$title,$rating,$runtime,$director,$dateOfRelease);
 
 //A Silent Voice
 //Personal rating 11/10
-$title="A Silent Voice";
-$rating="PG-13";
-$runtime=130;
-$director="Naoko Yamada";
-$dateOfRelease="2016-09-17";
-$movie->execute();
+movie->execute(array("A Silent Voice","PG-13",130,"Naoko Yamada","2016-09-17"));
 
 //Monty Python and the Holy Grail
 //Personal Rating 8/10
-$title="Monty Python and the Holy Grail";
-$rating="PG";
-$runtime=91;
-$director="Terry Gilliam";
-$dateOfRelease="1975-04-03";
-$movie->execute();
+$movie->execute(array("Monty Python and the Holy Grail","PG",91,"Terry Gilliam","1975-04-03"));
 
 //Back to the Future
 //Personal Rating 7/10
-$title="Back to the Future";
-$rating="PG";
-$runtime=116;
-$director="Robert Zemeckis";
-$dateOfRelease="1985-07-03";
-$movie->execute();
+$movie->execute(array("Back to the Future","PG",116,"Robert Zemeckis","1985-07-03"));
 
 //Iron Lung
 //Personal Rating N/A Plan to see it
-$title="Iron Lung";
-$rating="R";
-$runtime=110;
-$director="Mark Fischbach";
-$dateOfRelease="2024-01-01";
-$movie->execute();
+$movie->execute(array("Iron Lung","R",110,"Mark Fischbach","2024-01-01"));
 
 //SpaceBalls
 //Personal Rating 9/10
-$title="SpaceBalls";
-$rating="R";
-$runtime=96;
-$director="Mel Brooks";
-$dateOfRelease="1987-06-24";
-$movie->execute();
+$movie->execute(array("SpaceBalls","R",96,"Mel Brooks","1987-06-24"));
 
 //Gets Data from Movies
 $select_data = $db->prepare("SELECT movieName FROM movies WHERE runtime < 120;");
 $select_data->execute();
-$result = $select_data->fetchall;
+$result = $select_data->fetchall();
 
 //Fetch data
-foreach($result as $movie){
+foreach($result as $movie => $row){
     echo "<div></div>";
 }
 ?>

3c330337c9ef6e372f3333f501aa12ca0977da90
Blaze8145 <149420309+Blaze8145@users.noreply.github.com>
2026-04-18 04:15:27 +0000

    feat added the other radio button

--- HW7.js
+++ HW7.js
@@ -1,7 +1,27 @@
 //Button used to show ow many times it was clicked
+//https://developer.mozilla.org/en-US/docs/Web/API/Element
 const button = document.querySelector("#counter");
 
 button.addEventListener("click", (event) => {
   button.textContent = `Click count: ${event.detail}`;
 });
 
+//Other Radio Buttons
+document.querySelectorAll('input[name="age"]').forEach(radio => {
+    radio.addEventListener("change", async () => {
+        const otherBox = document.getElementById("otherBox");
+        const output = document.getElementById("result");
+        if (document.getElementById("other").checked) {
+            otherBox.style.display = "block";
+            const response = await fetch("getMessage.php");
+            const data = await response.text();
+            output.textContent = data;
+        } else {
+            otherBox.style.display = "none";
+            output.textContent = "";
+        }
+
+    });
+});
+
+//Email Validator
